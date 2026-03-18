ffdee8640981270fe745a26e07bc47fcb7cee39e
senthilnathan <pranavleaps@gmail.com>
2026-03-17 20:03:09 -0700

    Red autos w/out subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reyansh/Subsystems/TurretOdo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reyansh/Subsystems/TurretOdo.java
@@ -1,26 +1,26 @@
-package org.firstinspires.ftc.teamcode.Reyansh.Subsystems;
-
-import com.pedropathing.geometry.PedroCoordinates;
-import com.pedropathing.geometry.Pose;
-
-import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-
-import dev.nextftc.core.subsystems.Subsystem;
-
-public class TurretOdo implements Subsystem {
-
-
-    public static TurretOdo INSTANCE = new TurretOdo();
-    Pose cachedPose = null;
-    double x = 0;
-    double y = 0;
-    double heading = 0;
-    @Override
-    public void periodic() {
-
-
-        cachedPose = PedroComponent.follower().getPose();
-
-        x = cachedPose.getY();
-    }
-}
+//package org.firstinspires.ftc.teamcode.Reyansh.Subsystems;
+//
+//import com.pedropathing.geometry.PedroCoordinates;
+//import com.pedropathing.geometry.Pose;
+//
+//import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+//
+//import dev.nextftc.core.subsystems.Subsystem;
+//
+//public class TurretOdo implements Subsystem {
+//
+//
+//    public static TurretOdo INSTANCE = new TurretOdo();
+//    Pose cachedPose = null;
+//    double x = 0;
+//    double y = 0;
+//    double heading = 0;
+//    @Override
+//    public void periodic() {
+//
+//
+//        cachedPose = PedroComponent.follower().getPose();
+//
+//        x = cachedPose.getY();
+//    }
+//}
